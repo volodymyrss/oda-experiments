212e990b15b4bad811ede8bdd643acc8a2745295
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-04-23 21:17:00 +0200

    verify urls

--- odatestsapp.py
+++ odatestsapp.py
@@ -382,7 +382,7 @@ def midnight_timestamp():
 
 def recent_timestamp():
     sind = datetime.datetime.now().timestamp() - midnight_timestamp()
-    return midnight_timestamp() + int(sind/3600)*3600
+    return midnight_timestamp() + int(sind/36000)*36000
 
 @app.route('/offer-goal')
 def offer_goal():
